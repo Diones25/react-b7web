49982ee1d53c740c836276bc5c61a45116c2e24d
Diones <pereiradiones987@gmail.com>
2024-05-31 09:58:03 -0300

    voltando default

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Chat from "./components/Chat"
-import Galeria from "./components/Galeria"
 import { ChatProvider } from "./contexts/ChatContext"
 import { UserProvider } from "./contexts/UserContext"
 
@@ -7,7 +6,15 @@ const Page = () => {
 
   return (
     <>
-      <Galeria />
+      <div className="container mx-auto max-w-lg px-2">
+        <UserProvider>
+          <ChatProvider>
+            <h1 className="text-3xl my-3 text-center">Chat simples</h1>
+
+            <Chat />
+          </ChatProvider>
+        </UserProvider>
+      </div>
     </>
   )
 }
