c72d5cd4c956fbc3923f58ca686b31f7e9165d98
Diones P. Alves <pereiradiones987@gmail.com>
2024-12-17 14:43:48 -0300

    Fazendo um form básico

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
+import { SubmitHandler, useForm } from "react-hook-form"
 
+type Inputs = {
+  name: string;
+  lastName: string;
+}
 
 const Page = () => {
+  const { handleSubmit, register } = useForm<Inputs>();
+
+  const handleFormSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data)
+  }
 
   return (
     <>
       <div className="container mx-auto">
-        
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
+
+          <input
+            {...register('name')}
+            className="border border-white p-3 text-black"
+            placeholder="Digite seu nome"
+          />
+
+          <input
+            {...register('lastName')}
+            className="block mt-4 border border-white p-3 text-black"
+            placeholder="Digite seu sobrenome"
+          />
+
+          <input type="submit" value="Enviar" />
+
+        </form>
       </div>
     </>
   )
