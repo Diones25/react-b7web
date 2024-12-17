ba4bd9787a144b0d3356c5e22824171a37c5129a
Diones P. Alves <pereiradiones987@gmail.com>
2024-12-17 14:24:56 -0300

    react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import Chat from "./components/Chat"
-import { ChatProvider } from "./contexts/ChatContext"
-import { UserProvider } from "./contexts/UserContext"
+
 
 const Page = () => {
 
   return (
     <>
-      <div className="container mx-auto max-w-lg px-2">
-        <UserProvider>
-          <ChatProvider>
-            <h1 className="text-3xl my-3 text-center">Chat simples</h1>
-
-            <Chat />
-          </ChatProvider>
-        </UserProvider>
+      <div className="container mx-auto">
+        
       </div>
     </>
   )
