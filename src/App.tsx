82ac75e5225b61bd12d4119b028a5d63fbfcb518
Diones P. Alves <pereiradiones987@gmail.com>
2024-12-18 14:36:50 -0300

    Testando o Schema no Zod

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,25 @@ import { z } from 'zod';
 
 const Page = () => {
 
-  type signUpForm = {
+  type SignUpForm = {
     name: string;
     lastName: string;
     age: number;
   }
 
-  const signUpForm = z.object({
+  const SignUpForm = z.object({
     name: z.string().min(2).max(20),
     lastname: z.string().min(2).optional(),
     age: z.number().min(18)
   });
+
+  console.log(
+  SignUpForm.parse({
+    name: 'Diones',
+    lastname: 'Alves',
+    age: 25
+  })
+)
 
   return (
     <>
