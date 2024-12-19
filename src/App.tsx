be070d162a4d1ae063c734ff081b9c221d72f0ad
Diones P. Alves <pereiradiones987@gmail.com>
2024-12-19 11:45:05 -0300

    Definindo mensagem de erro no Zod

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { SignUpFormSchema } from "./schemas/SignUpFormSchema";
 
 
 const Page = () => {
-  const { register, handleSubmit } = useForm({
+  const { register, handleSubmit, formState: { errors } } = useForm({
     resolver: zodResolver(SignUpFormSchema)
   });
 
@@ -16,21 +16,33 @@ const Page = () => {
     <>
       <div className="container mx-auto">
         <form onSubmit={handleSubmit(handleSignUpForm)}>
-          <input
-            {...register('name')}
-            className="border border-white p-3 m-3 text-black"
-            placeholder="Digite seu nome"
-          />
-          <input
-            {...register('lastName')}
-            className="border border-white p-3 m-3 text-black"
-            placeholder="Digite seu sobrenome"
-          />
-          <input
-            {...register('age', { valueAsNumber: true })}
-            className="border border-white p-3 m-3 text-black"
-            placeholder="Digite sua idade"
-          />
+          <div>
+            <input
+              {...register('name')}
+              className="border border-white p-3 m-3 text-black"
+              placeholder="Digite seu nome"
+            />
+            {errors.name && <p className="text-red-500">{ errors.name.message as string }</p>}
+          </div>
+
+          <div>
+            <input
+              {...register('lastName')}
+              className="border border-white p-3 m-3 text-black"
+              placeholder="Digite seu sobrenome"
+            />
+            {errors.lastName && <p className="text-red-500">{errors.lastName.message as string}</p>}
+          </div>
+
+          <div>
+            <input
+              {...register('age', { valueAsNumber: true })}
+              className="border border-white p-3 m-3 text-black"
+              placeholder="Digite sua idade"
+            />
+            {errors.age && <p className="text-red-500">{errors.age.message as string}</p>}
+          </div>
+
           <input
             type="submit"
             value="Cadastrar"
--- src/schemas/SignUpFormSchema.ts
+++ src/schemas/SignUpFormSchema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const SignUpFormSchema = z.object({
-  name: z.string().min(2).max(20),
+  name: z.string().min(2, { message: 'Precisa ter pelo menos dois caracteres' }).max(20, { message: 'Pode ter no máximo 20 caracteres' }),
   lastName: z.string().optional(),
-  age: z.number().min(18)
+  age: z.number({ invalid_type_error: 'Precisa ser um número' }).min(18, { message: 'Precisa ser maior de 18 anos' })
 });
